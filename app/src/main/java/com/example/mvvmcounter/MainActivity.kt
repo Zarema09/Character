fe57150b550974e2561027e6e265bd868c5dfe5e
Zarema09 <omoraalieva.09@icloud.com>
2025-01-14 19:31:50 +0600

    -

--- app/src/main/java/com/example/mvvmcounter/MainActivity.kt
+++ app/src/main/java/com/example/mvvmcounter/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
-
+//git
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
